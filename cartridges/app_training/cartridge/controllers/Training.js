602efcdafe6b44fd3ee136991e3a9cf19949d316
Yan Oliveira <50741910+yanoliveira16@users.noreply.github.com>
2022-04-18 21:31:00 -0300

    4

--- cartridges/app_training/cartridge/controllers/Training.js
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -1,9 +1,13 @@
 'use strict';
-
+ 
 var server = require('server');
-
-server.extend(module.superModule)
-
+ 
+ 
+server.get('HelloWorld', function (req, res, next) {
+    var myvariable = "Just a string"
+    res.render("training/myfirsttemplate", { myvariable: myvariable });
+    return next();
+});
+ 
+ 
 module.exports = server.exports();
-
-next();
